f8eb54a463a987a83f07b993e2ed7dd17859497f
Wild Carrot <edelman.eliz@gmail.com>
2022-12-07 11:31:42 -0700

    initial commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,38 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
+const inquirer = require("inquirer");
+const fs = require("fs");
 const questions = [];
 
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What languages do you know?",
+      name: "languages",
+    },
+    {
+      type: "input",
+      message: "What is your preferred method of communication",
+      name: "comms",
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+
+    fs.writeFile("log.txt", JSON.stringify(answers), (err) =>
+      err ? console.error(err) : console.log(answers.name)
+    );
+  });
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
