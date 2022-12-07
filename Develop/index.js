9047d58847e4d3bcf5c68eb4e780935f69d7b71b
Wild Carrot <edelman.eliz@gmail.com>
2022-12-07 11:47:35 -0700

    Questions added and some beginning functions

--- Develop/index.js
+++ Develop/index.js
@@ -6,25 +6,40 @@ inquirer
   .prompt([
     {
       type: "input",
-      message: "What is your name?",
-      name: "name",
+      message: "What is your project name?",
+      name: "title",
     },
     {
       type: "input",
-      message: "What languages do you know?",
-      name: "languages",
+      message: "Write a short description of the project.",
+      name: "description",
     },
     {
       type: "input",
-      message: "What is your preferred method of communication",
-      name: "comms",
+      message: "What license would you like to use/list?",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "What would you like to include under the usage section?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "What would you like to include under the installation section?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Would you like to include any credits?",
+      name: "credits",
     },
   ])
   .then((answers) => {
     console.log(answers);
 
-    fs.writeFile("log.txt", JSON.stringify(answers), (err) =>
-      err ? console.error(err) : console.log(answers.name)
+    fs.writeFile("README.md", JSON.stringify(answers), (err) =>
+      err ? console.error(err) : console.log("README successfully created!")
     );
   });
 
